ec90477885a1533cbb7095890abcfa02488577aa
Sadel Muwahed <sad20200232@std.psut.edu.jo>
2024-04-25 00:46:16 +0300

    Added new tables

--- Asthmaguard/DataSource/Endpoints/Database/DatabaseSetUp.swift
+++ Asthmaguard/DataSource/Endpoints/Database/DatabaseSetUp.swift
@@ -6,11 +6,10 @@
 //
 
 import Foundation
-import UIKit
 import SQLite3
 
 class SQLiteDatabase {
-    var dbPointer: OpaquePointer?
+    private var dbPointer: OpaquePointer?
 
     private init(dbPointer: OpaquePointer?) {
         self.dbPointer = dbPointer
@@ -52,19 +51,16 @@ class SQLiteDatabase {
         }
     }
 
-    func executeQuery(sql: String) throws -> Int {
-        var queryStatement: OpaquePointer?
-        if sqlite3_prepare_v2(dbPointer, sql, -1, &queryStatement, nil) == SQLITE_OK {
-            if sqlite3_step(queryStatement) == SQLITE_DONE {
-                let lastId = Int(sqlite3_last_insert_rowid(dbPointer))
-                sqlite3_finalize(queryStatement)
-                return lastId
-            } else {
-                throw NSError(domain: "SQLiteError", code: 2, userInfo: [NSLocalizedDescriptionKey: "Execution failed"])
+    func executeInsert(sql: String) throws -> Int {
+        var errMsg: UnsafeMutablePointer<Int8>?
+        if sqlite3_exec(dbPointer, sql, nil, nil, &errMsg) != SQLITE_OK {
+            if let errMsg = errMsg {
+                let message = String(cString: errMsg)
+                sqlite3_free(errMsg)
+                throw NSError(domain: "SQLiteError", code: 1, userInfo: [NSLocalizedDescriptionKey: message])
             }
-        } else {
-            throw NSError(domain: "SQLiteError", code: 2, userInfo: [NSLocalizedDescriptionKey: "Query preparation fail"])
         }
+        return Int(sqlite3_last_insert_rowid(dbPointer))
     }
 }
 
@@ -88,7 +84,8 @@ class DatabaseManager {
             Username TEXT,
             Password TEXT,
             Email TEXT,
-            CreationDate TEXT
+            CreationDate TEXT,
+            AppleID TEXT
         );
         """
 
@@ -104,8 +101,49 @@ class DatabaseManager {
             FOREIGN KEY (UserID) REFERENCES Users(UserID)
         );
         """
+
+        let createAsthmaTriggersTableSQL = """
+        CREATE TABLE IF NOT EXISTS AsthmaTriggers (
+            TriggerID INTEGER PRIMARY KEY,
+            PatientID INTEGER,
+            Grade INTEGER,
+            FOREIGN KEY (PatientID) REFERENCES PatientInformation(PatientID)
+        );
+        """
+
+        let createTriggersTableSQL = """
+        CREATE TABLE IF NOT EXISTS Triggers (
+            TriggerID INTEGER PRIMARY KEY,
+            TriggerName TEXT
+        );
+        """
+
+        let createBiometricDataTableSQL = """
+        CREATE TABLE IF NOT EXISTS BiometricData (
+            Serial INTEGER PRIMARY KEY,
+            TriggerID INTEGER,
+            Value INTEGER,
+            FOREIGN KEY (TriggerID) REFERENCES Triggers(TriggerID)
+        );
+        """
+
+        let createAsthmaTreatmentsTableSQL = """
+        CREATE TABLE IF NOT EXISTS AsthmaTreatments (
+            PatientID INTEGER,
+            TreatmentID INTEGER PRIMARY KEY,
+            TreatmentDescription TEXT,
+            Dosage INTEGER,
+            Frequency TEXT,
+            FOREIGN KEY (PatientID) REFERENCES PatientInformation(PatientID)
+        );
+        """
+
         try database.createTable(sql: createUsersTableSQL)
         try database.createTable(sql: createPatientInformationTableSQL)
+        try database.createTable(sql: createAsthmaTriggersTableSQL)
+        try database.createTable(sql: createTriggersTableSQL)
+        try database.createTable(sql: createBiometricDataTableSQL)
+        try database.createTable(sql: createAsthmaTreatmentsTableSQL)
     }
 
     func addUser(username: String, password: String, email: String) -> Int? {
@@ -119,7 +157,7 @@ class DatabaseManager {
         VALUES ('\(username)', '\(password)', '\(email)', '\(creationDate)');
         """
         do {
-            let userID = try database.executeQuery(sql: userSQL)
+            let userID = try database.executeInsert(sql: userSQL)
             print("User added successfully with UserID = \(userID).")
             return userID
         } catch {
@@ -128,51 +166,73 @@ class DatabaseManager {
         }
     }
 
-    func fetchUsers() {
-        let querySQL = "SELECT UserID, Username, Password, Email, CreationDate, AppleID FROM Users;"
-        do {
-            var queryStatement: OpaquePointer? = nil
-            if sqlite3_prepare_v2(database.dbPointer, querySQL, -1, &queryStatement, nil) == SQLITE_OK {
-                while sqlite3_step(queryStatement) == SQLITE_ROW {
-                    let userID = sqlite3_column_int(queryStatement, 0)
-                    guard let usernameQueryResult = sqlite3_column_text(queryStatement, 1) else { continue }
-                    let username = String(cString: usernameQueryResult)
-                    guard let passwordQueryResult = sqlite3_column_text(queryStatement, 2) else { continue }
-                    let password = String(cString: passwordQueryResult)
-                    guard let emailQueryResult = sqlite3_column_text(queryStatement, 3) else { continue }
-                    let email = String(cString: emailQueryResult)
-                    guard let creationDateQueryResult = sqlite3_column_text(queryStatement, 4) else { continue }
-                    let creationDate = String(cString: creationDateQueryResult)
-                    guard let appleIDQueryResult = sqlite3_column_text(queryStatement, 5) else { continue }
-                    let appleID = String(cString: appleIDQueryResult)
-                    print("User ID: \(userID), Username: \(username), Password: \(password), Email: \(email), Creation Date: \(creationDate), Apple ID: \(appleID)")
-                }
-                sqlite3_finalize(queryStatement)
-            } else {
-                let errmsg = String(cString: sqlite3_errmsg(database.dbPointer))
-                print("Error preparing select statement: \(errmsg)")
-            }
-        } catch {
-            print("Error fetching users: \(error)")
-        }
-    }
-
-
     func addPatient(name: String, dob: Date, gender: Bool, height: Float, weight: Float, userID: Int) {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
         let dobString = dateFormatter.string(from: dob)
-        
+
         let patientSQL = """
         INSERT INTO PatientInformation (UserID, Name, DateOfBirth, Gender, Height, Weight)
         VALUES (\(userID), '\(name)', '\(dobString)', \(gender ? 1 : 0), \(height), \(weight));
         """
+
         do {
-            try database.executeQuery(sql: patientSQL)
+            try database.executeInsert(sql: patientSQL)
             print("Patient added successfully.")
         } catch {
             print("Error adding patient: \(error)")
         }
     }
 
+    func insertTrigger(triggerID: Int, triggerName: String) {
+        let triggerSQL = """
+        INSERT INTO Triggers (TriggerID, TriggerName)
+        VALUES (\(triggerID), '\(triggerName)');
+        """
+        do {
+            try database.executeInsert(sql: triggerSQL)
+            print("Trigger added successfully.")
+        } catch {
+            print("Error adding trigger: \(error)")
+        }
+    }
+
+    func insertAsthmaTrigger(triggerID: Int, patientID: Int, grade: Int) {
+        let asthmaTriggerSQL = """
+        INSERT INTO AsthmaTriggers (TriggerID, PatientID, Grade)
+        VALUES (\(triggerID), \(patientID), \(grade));
+        """
+        do {
+            try database.executeInsert(sql: asthmaTriggerSQL)
+            print("Asthma Trigger added successfully.")
+        } catch {
+            print("Error adding asthma trigger: \(error)")
+        }
+    }
+
+    func insertBiometricData(serial: Int, triggerID: Int, value: Int) {
+        let biometricDataSQL = """
+        INSERT INTO BiometricData (Serial, TriggerID, Value)
+        VALUES (\(serial), \(triggerID), \(value));
+        """
+        do {
+            try database.executeInsert(sql: biometricDataSQL)
+            print("Biometric Data added successfully.")
+        } catch {
+            print("Error adding biometric data: \(error)")
+        }
+    }
+
+    func insertAsthmaTreatment(patientID: Int, treatmentID: Int, treatmentDescription: String, dosage: Int, frequency: String) {
+        let asthmaTreatmentSQL = """
+        INSERT INTO AsthmaTreatments (PatientID, TreatmentID, TreatmentDescription, Dosage, Frequency)
+        VALUES (\(patientID), \(treatmentID), '\(treatmentDescription)', \(dosage), '\(frequency)');
+        """
+        do {
+            try database.executeInsert(sql: asthmaTreatmentSQL)
+            print("Asthma Treatment added successfully.")
+        } catch {
+            print("Error adding asthma treatment: \(error)")
+        }
+    }
 }
