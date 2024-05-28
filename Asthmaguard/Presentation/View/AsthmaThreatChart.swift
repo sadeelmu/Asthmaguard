0c58b1bff4a9c8e5f4b73911ce5dfdabd013b816
Sadel Muwahed <sad20200232@std.psut.edu.jo>
2024-05-28 19:10:03 +0300

    feat: alerts in dashboard

---
+++ Asthmaguard/Presentation/View/AsthmaThreatChart.swift
@@ -0,0 +1,111 @@
+//
+//  AsthmaThreatChart.swift
+//  Asthmaguard
+//
+//  Created by Sadeel Muwahed on 28/05/2024.
+//
+
+import Foundation
+import SwiftUI
+import Charts
+
+@available(iOS 17.0, *)
+struct AsthmaThreatChart: View {
+    @State private var totalWeightedSeverity: Double = 0.0
+    @State private var biosignalRisk: Double = 0.0
+    @State private var environmentalRisk: Double = 0.0
+    @State private var showHighThreatAlert: Bool = false
+    @State private var showVeryHighThreatActionSheet: Bool = false
+
+    @State var asthmathreat: [AsthmaThreat] = [
+        .init(title: "Biosignals", risks: 0.3),
+        .init(title: "Environmental", risks: 0.1),
+        .init(title: "Normal", risks: 0.6)
+    ]
+
+    private let asthmaThreatCalculatorUseCase = AsthmaThreatCalculatorUseCase()
+
+    func updateAsthmaThreat() {
+        asthmaThreatCalculatorUseCase.fetchDataAndCalculateAsthmaSeverity()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            // Use a delay to allow for data fetching; adjust timing as necessary
+            self.biosignalRisk = asthmaThreatCalculatorUseCase.weightedBioSignalRisk
+            self.environmentalRisk = asthmaThreatCalculatorUseCase.weightedEnvironmentalRisk
+            self.totalWeightedSeverity = asthmaThreatCalculatorUseCase.totalWeightedSeverity
+
+            if totalWeightedSeverity > 0.75 {
+                showVeryHighThreatActionSheet = true
+            } else if totalWeightedSeverity > 0.50 {
+                showHighThreatAlert = true
+            }
+        }
+    }
+
+    @State private var showDetailsView = false
+
+    var body: some View {
+        ScrollView(showsIndicators: false) {
+            VStack(spacing: 20) {
+                Text("Dashboard")
+                    .font(Font.custom("Poppins-Bold", size: 18))
+                    .padding(.all)
+
+                let chartColors: [Color] = [
+                    .pink.opacity(0.5), .pink, .blue
+                ]
+
+                Text("Asthma Threat: \(totalWeightedSeverity * 100, specifier: "%.1f")%")
+                    .font(Font.custom("Poppins-Bold", size: 26))
+
+                Chart(asthmathreat) { asthmathreat in
+                    SectorMark(
+                        angle: .value(
+                            Text(asthmathreat.title),
+                            asthmathreat.risks
+                        ),
+                        innerRadius: .ratio(0.6),
+                        angularInset: 0.8
+                    )
+                    .cornerRadius(4)
+                    .foregroundStyle(by: .value("Threats", asthmathreat.title))
+                }.frame(width: 300, height: 250)
+                .chartForegroundStyleScale(domain: .automatic, range: chartColors)
+
+                Spacer()
+                VStack(spacing: 10) {
+                    CustomBioData(bioSignal: "Respiratory Rate", time: "11:55 AM", data: "19 breaths/min")
+                    CustomBioData(bioSignal: "Heart Rate", time: "12:01 PM", data: "96 BPM")
+                    CustomBioData(bioSignal: "Blood Oxygen", time: "12:03 PM", data: "98.5%")
+                }
+                Spacer()
+            }
+            .onAppear() {
+                updateAsthmaThreat()
+            }
+            .alert(isPresented: $showHighThreatAlert) {
+                Alert(
+                    title: Text("High asthma threat detected"),
+                    primaryButton: .default(Text("Contact emergency contact")) {
+                        // Handle contact emergency action
+                    },
+                    secondaryButton: .cancel(Text("Dismiss"))
+                )
+            }
+            .actionSheet(isPresented: $showVeryHighThreatActionSheet) {
+                ActionSheet(
+                    title: Text("Very High Asthma threat detected!"),
+                    message: Text("Would you like to start breathing exercises?"),
+                    buttons: [
+                        .default(Text("Start")) {
+                            // Handle start breathing exercises action
+                        },
+                        .default(Text("Contact emergency contact")) {
+                            // Handle contact emergency action
+                        },
+                        .cancel(Text("Dismiss"))
+                    ]
+                )
+            }
+        }
+    }
+}
--- Asthmaguard/Presentation/View/DashboardView.swift
+++ Asthmaguard/Presentation/View/DashboardView.swift
@@ -10,111 +10,17 @@ import SwiftUI
 import Charts
 import CoreLocation
 
-@available(iOS 17.0, *)
-struct AsthmaThreatChart: View {
-    @State private var totalWeightedSeverity: Double = 0.0
-    @State private var biosignalRisk: Double = 0.0
-    @State private var environmentalRisk: Double = 0.0
-    @State private var normal: Double = 0.0
-
-    @State var asthmathreat: [AsthmaThreat] = [
-        .init(title: "Biosignals", risks: 0.0),
-        .init(title: "Enviromental", risks: 0.0),
-        .init(title: "Normal", risks: 0.0)
-    ]
-
-    private let asthmaThreatCalculatorUseCase = AsthmaThreatCalculatorUseCase()
-
-    func updateAsthmaThreat() {
-        asthmaThreatCalculatorUseCase.fetchDataAndCalculateAsthmaSeverity()
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-            // Use a delay to allow for data fetching; adjust timing as necessary
-            self.biosignalRisk = asthmaThreatCalculatorUseCase.weightedBioSignalRisk
-            self.environmentalRisk = asthmaThreatCalculatorUseCase.weightedEnvironmentalRisk
-            self.totalWeightedSeverity = asthmaThreatCalculatorUseCase.totalWeightedSeverity
-            self.normal = 1 - self.totalWeightedSeverity
-
-            self.updateAsthmaThreatArray()
-        }
-    }
-
-    func updateAsthmaThreatArray() {
-        self.asthmathreat = [
-            .init(title: "Biosignals", risks: self.biosignalRisk),
-            .init(title: "Enviromental", risks: self.environmentalRisk),
-            .init(title: "Normal", risks: self.normal)
-        ]
-    }
-
-    @State private var showDetailsView = false
-
-    var body: some View {
-        ScrollView(showsIndicators: false) {
-            VStack(spacing: 20) {
-                Text("Dashboard")
-                    .font(Font.custom("Poppins-Bold", size: 18))
-                    .padding(.all)
-
-                let chartColors: [Color] = [
-                    .pink.opacity(0.5), .pink, .blue
-                ]
-
-                Text("Asthma Threat: \(totalWeightedSeverity * 100, specifier: "%.1f")%")
-                    .font(Font.custom("Poppins-Bold", size: 26))
-
-                Chart(asthmathreat) { asthmathreat in
-                    SectorMark(
-                        angle: .value(
-                            Text(asthmathreat.title),
-                            asthmathreat.risks
-                        ),
-                        innerRadius: .ratio(0.6),
-                        angularInset: 0.8
-                    )
-                    .cornerRadius(4)
-                    .foregroundStyle(by: .value("Threats", asthmathreat.title))
-                }.frame(width: 300, height: 250)
-                .chartForegroundStyleScale(domain: .automatic, range: chartColors)
-
-                Spacer()
-                VStack(spacing: 10) {
-                    CustomBioData(bioSignal: "Respiratory Rate", time: "11:55 AM", data: "19 breathes/min")
-                    CustomBioData(bioSignal: "Heart Rate", time: "12:01 PM", data: "96 BPM")
-                    CustomBioData(bioSignal: "Blood Oxygen", time: "12:03 PM", data: "98.5%")
-                }
-                Spacer()
-            }
-            .onAppear() {
-                updateAsthmaThreat()
-            }
-        }
-    }
-}
-
 
 @available(iOS 17.0, *)
 struct DashboardView: View {
-
     @State private var selection = 0
-    private let locationManager = LocationManager.shared
-    private let asthmaThreatCalculatorUseCase = AsthmaThreatCalculatorUseCase()
     
     var body: some View {
-        TabView(selection:$selection) {
+        TabView(selection: $selection) {
             NavigationView {
                 AsthmaThreatChart()
                     .navigationTitle("")
                     .navigationBarBackButtonHidden(false)
-                    .onAppear {
-                        locationManager.requestLocation()
-                        BioSignalData.requestHealthDataAccessIfNeeded { success in
-                            if success {
-                                asthmaThreatCalculatorUseCase.startMonitoring()
-                            } else {
-                                print("Health data access denied.")
-                            }
-                        }
-                    }
             }.tabItem {
                 Label("Dashboard", systemImage: "house")
             }
@@ -126,7 +32,7 @@ struct DashboardView: View {
                 }
                 .tag(1)
             
-            BreathingExerciseScreen().tabItem { Label("Breathing", systemImage:"figure.mind.and.body")}.tag(2)
+            BreathingExerciseScreen().tabItem { Label("Breathing", systemImage: "figure.mind.and.body") }.tag(2)
             
             SettingsScreen().tabItem { Label("Settings", systemImage: "gearshape") }.tag(3)
         }
@@ -134,8 +40,9 @@ struct DashboardView: View {
 }
 
 
+
 @available(iOS 17.0, *)
-struct AsthmaThreatChart_Previews: PreviewProvider {
+struct DashboardView_Previews: PreviewProvider {
     static var previews: some View {
         DashboardView()
     }
